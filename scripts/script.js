cdd75f3c583698a775c8c26dc59966a2644866a3
PabVF23 <UO282655@uniovi.es>
2024-11-13 13:21:17 +0100

    New Commit

---
+++ scripts/script.js
@@ -0,0 +1,106 @@
+function buscarTexto() {
+    let resultados = {};
+    let texto = $('input').val().toLowerCase()
+
+    console.log(!texto)
+
+    if (!texto) {
+        mostrarResultados(resultados)
+        return;
+    }
+
+    let sobreMiFetch = fetch("../sobremi.html").then(response => response.text())
+    let aficionesFetch = fetch("../aficiones.html").then(response => response.text())
+    let proyectosFetch = fetch("../proyectos.html").then(response => response.text())
+
+    Promise.all([sobreMiFetch, aficionesFetch, proyectosFetch])
+    .then(results => {
+        var [sobreMiData, aficionesData, proyectosData] = results
+
+        var sobreMiEntries = []
+
+        $(sobreMiData).find("p, h1, h2, h3, li").each(function() {
+            let text = $(this).text()
+            let textLower = text.toLowerCase()
+            if (textLower.includes(texto)) {
+                sobreMiEntries.push(text)
+            }
+        })
+
+        if (sobreMiEntries.length) {
+            resultados["sobremi"] = sobreMiEntries
+        }
+
+        var aficionesEntries = []
+
+        $(aficionesData).find("p, h1, h2, h3, li").each(function() {
+            let text = $(this).text()
+            let textLower = text.toLowerCase()
+            if (textLower.includes(texto)) {
+                aficionesEntries.push(text)
+            }
+        })
+
+        if (aficionesEntries.length) {
+            resultados["aficiones"] = aficionesEntries
+        }
+
+        var proyectosEntries = []
+
+        $(proyectosData).find("p, h1, h2, h3, li").each(function() {
+            let text = $(this).text()
+            let textLower = text.toLowerCase()
+            if (textLower.includes(texto)) {
+                proyectosEntries.push(text)
+            }
+        })
+
+        if (proyectosEntries.length) {
+            resultados["proyectos"] = proyectosEntries
+        }
+
+        mostrarResultados(resultados);
+    })
+}
+
+function mostrarResultados(resultados) {
+    let contenedorResultados = $("article").last()
+    $(contenedorResultados).empty()
+
+    if (Object.keys(resultados).length) {
+        $(contenedorResultados).append("<h3>Resultados</h3>")
+    }
+
+    if (resultados["sobremi"]) {
+        $(contenedorResultados).append("<p>Sobre mí:</p>")
+        $(contenedorResultados).append("<ul></ul>")
+
+        var list = $(contenedorResultados).find("ul").last()
+
+        resultados["sobremi"].forEach(r => {
+            $(list).append("<li>" + r + "</li>")
+        })
+    }
+
+    if (resultados["aficiones"]) {
+        $(contenedorResultados).append("<p>Mis aficiones:</p>")
+        $(contenedorResultados).append("<ul></ul>")
+
+        var list = $(contenedorResultados).find("ul").last()
+
+        resultados["aficiones"].forEach(r => {
+            $(list).append("<li>" + r + "</li>")
+        })
+    }
+
+    if (resultados["proyectos"]) {
+        $(contenedorResultados).append("<p>Mis proyectos:</p>")
+        $(contenedorResultados).append("<ul></ul>")
+
+        var list = $(contenedorResultados).find("ul").last()
+
+        resultados["proyectos"].forEach(r => {
+            $(list).append("<li>" + r + "</li>")
+        })
+    }
+}
